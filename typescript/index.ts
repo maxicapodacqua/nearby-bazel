aa5d5daae5cc3adeda37098700544432b11db870
maxicapodacqua <maxi.capodacqua@gartner.com>
2023-04-12 11:55:02 -0500

    Running server

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,14 @@
-console.log('Some log from typescript');
+import http, {IncomingMessage, ServerResponse} from 'http';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req :IncomingMessage, res :ServerResponse) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
